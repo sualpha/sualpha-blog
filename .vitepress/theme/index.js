d27d3be101d1ae2f381ea92fad5e88c5c22942af
subo <www.suso@qq.com>
2024-08-02 18:24:07 +0800

    Speed Insights

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,11 +1,13 @@
 // index.js
 import DefaultTheme from 'vitepress/theme'
 import Layout from './components/Layout.vue'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 import './custom.css'
 
 export default {
     extends: DefaultTheme,
     Layout: Layout,
+    SpeedInsights: SpeedInsights,
     enhanceApp({ app }) {
         app.provide('DEV', process.env.NODE_ENV === 'development')
       }
